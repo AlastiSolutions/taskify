581e1dc46f7af04fc653d719895d230431f946cc
Kourosh Alasti <coding@kouroshalasti.com>
2023-12-06 14:45:32 -0800

    Update page.tsx
    create login form

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,22 @@
+import { AuthFormType } from "@/constants/auth.types";
+import AuthFormField from "../AuthFormField";
+
 const LoginPage: React.FC = () => {
   return (
     <>
-      <div className="container bg-white text-slate-800 rounded-md p-4 mx-auto">
-        <h1>Username</h1>
-        <h1>Password</h1>
+      <div className="container flex flex-col items-centerd bg-white text-slate-800 rounded-md p-6 mt-12 mx-auto w-[50%] shadow-md">
+        <AuthFormField
+          inputName="email"
+          inputId="email"
+          formType={AuthFormType.EMAIL}
+          label="Email"
+        />
+        <AuthFormField
+          inputName="password"
+          inputId="password"
+          formType={AuthFormType.PASSWORD}
+          label="Password"
+        />
       </div>
     </>
   );
